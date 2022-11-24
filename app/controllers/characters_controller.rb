27cf6997161c0b7340b679bc221d47d4c7b1bd09
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2022-11-24 16:39:53 +0100

    create and save character to database

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -14,6 +14,17 @@ class CharactersController < ApplicationController
     end
 
     def create
+        @character = Character.new 
+        @character.name = params[:character][:name]
+        @character.exp = params[:character][:exp]
+        @character.strength = 1 
+        @character.dexterity = 1
+
+        if @character.save
+            redirect_to character_path
+          else
+            render :new, status: :unprocessable_entity
+          end
 
     end
 
