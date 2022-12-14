04df7d00e67a1b75e5c108e0ecd28cd80b6ced26
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2022-12-14 19:30:16 +0100

    classes images character creation radio

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,57 +1,53 @@
 class CharactersController < ApplicationController
-    def index
-        @characters = current_user.characters
+  def index
+    @characters = current_user.characters
+  end
+
+  def show
+    @character = if params[:id].nil?
+                   current_user.characters.first!
+                 else
+                   Character.find params[:id]
+                 end
+
+    @statistic = @character.statistic
+  end
+
+  def create
+    @character = Character.new
+    @character.user = current_user
+    @character.health = 100
+    @character.exp = 0
+    @character.level = 0
+
+    @character.name = params[:character][:name]
+    stat = Statistic.new default_statistic_params
+
+    @character.statistic = stat
+    if @character.save
+      redirect_to @character
+    else
+      render :new, status: :unprocessable_entity
     end
-    
-    def show 
+  end
 
-        if params[:id].nil?
-            @character = current_user.characters.first! 
-        else
-            @character = Character.find params[:id]
-        end
+  def new
+    @character = Character.new
+  end
 
-        @statistic = @character.statistic 
+  private
 
-    end
+  def default_statistic_params
+    {
+      strength: 1,
+      dexterity: 1,
+      stamina: 1,
+      initiative: 1,
 
-    def create
-        
-        @character = Character.new
-        @character.user = current_user
-        @character.health = 100
-        @character.exp = 0
-        @character.level = 0
-        
-        @character.name = params[:character][:name]
-        stat = Statistic.new default_statistic_params
-
-        @character.statistic = stat
-        if @character.save
-            redirect_to @character
-          else
-            render :new, status: :unprocessable_entity
-        end
-
-    end
-
-    def new
-        @character = Character.new
-    end
-
-    private
-    def default_statistic_params
-        return {  
-            strength: 1,
-            dexterity: 1, 
-            stamina: 1, 
-            initiative: 1, 
-            
-            added_strength: 0, 
-            added_dexterity: 0, 
-            added_stamina: 0, 
-            added_initiative: 0
-        }
-    end
-
+      added_strength: 0,
+      added_dexterity: 0,
+      added_stamina: 0,
+      added_initiative: 0
+    }
+  end
 end
