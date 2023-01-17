b44d89231fe113d9e4a0d631b99cdaa07743bc2a
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2023-01-17 22:28:18 +0100

    Films tests  title not null  director not null  title not longer than 100  correct convention

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,10 @@
 class Film < ApplicationRecord
   has_many :items, as: :rentable
+
+  validates :title, presence: true, length: {maximum: 100}
+  validates :director, presence: true, length: {maximum: 100}
+
+  # t.integer "publication_year"
+  # t.text "cast"
+
 end
