40e6ab4727d75376dd795a127844c28d53c339f0
Dawid Żyluk <91137753+Ronahon@users.noreply.github.com>
2023-01-18 11:15:39 +0100

    Update game_test.rb

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doesn't save game without a title" do
+    game = Game.new studio: 'CD Projekt', publication_year: 2015
+    assert_not game.save, 'Saved game without a title'
+  end
+
+  test "doesn't save game without studio" do
+    game = Game.new title: 'Wiedźmin 3', publication_year: 2015
+    assert_not game.save, 'Saved game without studio name'
+  end
+
+  test "doesn't save game without publication year" do
+    game = Game.new title: 'Wiedźmin 3', studio: 'CD Projekt'
+    assert_not game.save, 'Saved game without publication year'
+  end
+
+  test "doesn't save game with publication year in the future" do
+    game = Game.new title: 'Wiedźmin 3', studio: 'CD Projekt', publication_year: (Time.now + 2.years)
+    assert_not game.save, 'Saved game with a future publication year'
+  end
+
+  test "doesn't save game with publication year before 1900" do
+    game = Game.new title: 'Wiedźmin 3', studio: 'CD Projekt', publication_year: 1899
+    assert_not game.save, 'Saved game with publication year before 1900'
+  end
 end
