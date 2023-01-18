375f4d97fd179c9c6e9c6056416bf06e32d948a7
Dawid Żyluk <91137753+Ronahon@users.noreply.github.com>
2023-01-18 11:15:13 +0100

    Update game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   has_many :items, as: :rentable
+
+  validates :title, presence: true
+  validates :studio, presence: true
+  validates :publication_year, presence: true, inclusion: { in: 1900..Date.current.year }
 end
